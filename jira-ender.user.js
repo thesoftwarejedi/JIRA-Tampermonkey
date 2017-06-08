e7b254d0cb27357266144cba1b60d6ff89a7ec29
Dana Hanna <dana.hanna@gmail.com>
2017-06-08 15:05:34 -0400

    Update jira-ender.user.js

--- jira-ender.user.js
+++ jira-ender.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         JIRA Ender Extensions
 // @namespace    http://enderlabs.com/
-// @version      0.2
+// @version      0.3
 // @description  Do things I want JIRA to do
 // @author       @thesoftwarejedi
 // @match        http*://*.atlassian.net/secure/RapidBoard.jspa*
+// @match        http*://*.atlassian.net/browse/*
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/thesoftwarejedi/JIRA-Tampermonkey/master/jira-ender.user.js
 // @updateURL    https://raw.githubusercontent.com/thesoftwarejedi/JIRA-Tampermonkey/master/jira-ender.user.js
@@ -12,6 +13,9 @@
 
 (function() {
     'use strict';
+    
+    $('.external-link').attr('target', '_blank');
+    
     $('#ghx-plan').click(function() {
         updateTarget();
     });
@@ -23,6 +27,7 @@
 async function updateTarget() {
     await sleep(2000);
     $('#issuekey-val > a').attr('target', '_blank');
+    $('.external-link').attr('target', '_blank');
 }
 
 function sleep(ms) {
